1f8b7e1ac773f3cea7d5d0d5dfa056044bdf9d13
george <gibstep99@gmail.com>
2026-05-27 19:05:24 -0400

    remove test for depricated struct

--- test/runtests.jl
+++ test/runtests.jl
@@ -171,31 +171,6 @@ end
     display(vec(mean(prcnt_err, dims=(1, 3))))
 end
 
-# @testset "UniVarMomentsAccNDLabel tests" begin
-#     a = rand(20000, 20)
-#     l = rand(UInt8, 20000, 4)
-#     m1 = Moments.UniVarMomentsAcc{Float64, UInt8, Array}(10, 20, 256)
-#     m2 = Moments.UniVarMomentsAccNDLabel{Float64, UInt8, Array, 1}(10, 20, 256, (4, ))
-
-#     # initialization update
-#     Moments.centered_sum_update!(m1, a[1:10000, :], l[1:10000, 1])
-#     Moments.centered_sum_update!(m2, a[1:10000, :], l[1:10000, :])
-#     @test all(isapprox.(m1.moments, m2.moments[1, :, :, :]))
-#     println("Update 1 percent error by order:")
-#     prcnt_err = abs.((m1.moments .- m2.moments[1, :, :, :]) ./ m2.moments[1, :, :, :]).*100
-#     display(vec(mean(prcnt_err, dims=(1, 3))))
-
-
-#     # merge update
-#     Moments.centered_sum_update!(m1, a[10001:end, :], l[10001:end, 1])
-#     Moments.centered_sum_update!(m2, a[10001:end, :], l[10001:end, :])
-#     @test all(isapprox.(m1.moments, m2.moments[1, :, :, :]))
-#     println("Update 2 percent error by order:")
-#     prcnt_err = abs.((m1.moments .- m2.moments[1, :, :, :]) ./ m2.moments[1, :, :, :]).*100
-#     display(vec(mean(prcnt_err, dims=(1, 3))))
-
-# end
-
 @testset "Test that Chunked SNR is equivalent on dimension 2" begin
     t = rand(Float64, 5000, 1000)
     l = rand(UInt8, 5000)
