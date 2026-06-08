343ba5ad0639d9bd2dba0cba039b1e5906c6bb33
george <gibstep99@gmail.com>
2026-06-08 10:54:51 -0400

    _

--- profiling/profiling1.jl
+++ profiling/profiling1.jl
@@ -1,34 +1,39 @@
 using SCA 
 using CUDA
 using DataFrames, CSV
+using BenchmarkTools
+using Statistics
 
-function test_1(order::Int, ns::Int, nt::Int, ldomain, nl::Int)
+function test_1_CUDA(order::Int, ns::Int, nt::Int, ldomain, lsize::Int)
     t = CuArray(rand(Float32, nt, ns))
-    l = CuArray(UInt8.(rand(UInt8, nt, nl)))
+    l = CuArray(UInt8.(rand(UInt8, nt, lsize)))
     # m2 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, 8}(8, ns, 256)
     # m3 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, 16}(8, ns, 256)
-    m4 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, nl}(order, ns, 256)
+    m4 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, lsize}(order, ns, 256)
 
     CUDA.@profile Moments.centered_sum_update!(m4, t, l)
     # m4
 end
 
-function test_2()
-    t = rand(Float32, 100000, 1000)
-    l = rand(UInt8, 100000, 16)
-    m4 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, Array, 4}(8, 1000, 256)
-    @time "CPU moment update" Moments.centered_sum_update!(m4, t, l[:, 1:4])
+function test_1_CPU(order::Int, ns::Int, nt::Int, ldomain, lsize::Int)
+    t = CuArray(rand(Float32, nt, ns))
+    l = CuArray(UInt8.(rand(UInt8, nt, lsize)))
+    # m2 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, 8}(8, ns, 256)
+    # m3 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, 16}(8, ns, 256)
+    m4 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, CuArray, lsize}(order, ns, 256)
+
+    @benchmark Moments.centered_sum_update!($m4, $t, $l)
 end
 
-# test_1_wrapper("A5000-atomic-results", [2, 4, 8, 16, 32, 64], [100, 500, 1000], [10000, 20000, 50000, 100000], [2, 4, 8, 16, 32, 64, 128])
-# test_1_wrapper("A5000-non-atomic-results", [2, 4, 8, 16, 32, 64], [100, 500, 1000], [10000, 20000, 50000, 100000], [2, 4, 8, 16, 32, 64, 128])
-# test_1_wrapper("A5000-non-atomic-results-2", [2, 4, 8, 16, 32], [100, 200, 400, 800, 1600], [20000, ], [2, 4, 8, 16, 32, 64, 128, 256])
-function test_1_wrapper(output_name::String, orders::Vector{Int}, Ns::Vector{Int}, Nt::Vector{Int}, lsize::Vector{Int})
+# test_1_CUDA_wrapper("A5000-atomic-results", [2, 4, 8, 16, 32, 64], [100, 500, 1000], [10000, 20000, 50000, 100000], [2, 4, 8, 16, 32, 64, 128])
+# test_1_CUDA_wrapper("A5000-non-atomic-results", [2, 4, 8, 16, 32, 64], [100, 500, 1000], [10000, 20000, 50000, 100000], [2, 4, 8, 16, 32, 64, 128])
+# test_1_CUDA_wrapper("A5000-non-atomic-results-2", [2, 4, 8, 16, 32], [100, 200, 400, 800, 1600], [20000, ], [2, 4, 8, 16, 32, 64, 128, 256])
+function test_1_CUDA_wrapper(output_name::String, orders::Vector{Int}, Ns::Vector{Int}, Nt::Vector{Int}, lsize::Vector{Int})
     # result_file = open("profiling/internal-profiling/$(output_name).csv", "w")
     
     for (order, ns, nt, lsize) in Iterators.product(orders, Ns, Nt, lsize)
         println("Profiling for order=$(order) ns=$(ns) nt=$(nt) lsize=$(lsize)")
-        bench_result = test_1(order, ns, nt, 0:255, lsize)
+        bench_result = test_1_CUDA(order, ns, nt, 0:255, lsize)
         res_buf = IOBuffer()
         show(res_buf, bench_result)
         res_string = String(take!(res_buf))
@@ -44,6 +49,29 @@ function test_1_wrapper(output_name::String, orders::Vector{Int}, Ns::Vector{Int
             "# traces" => nt,
             "# labels" => lsize,
             "time" => gpu_time
+        )
+
+        CSV.write("profiling/internal-profiling/$(output_name).csv", results, append=true)
+    end
+end
+
+# test_1_CUDA_wrapper("threadripper-results-3", [2, 4, 8, 16, 32], [100, 200, 400, 800, 1600], [20000, ], [2, 4, 8, 16, 32, 64, 128, 256])
+function test_1_CPU_wrapper(output_name::String, orders::Vector{Int}, Ns::Vector{Int}, Nt::Vector{Int}, lsize::Vector{Int})
+    # result_file = open("profiling/internal-profiling/$(output_name).csv", "w")
+    
+    for (order, ns, nt, lsize) in Iterators.product(orders, Ns, Nt, lsize)
+        println("Profiling for order=$(order) ns=$(ns) nt=$(nt) lsize=$(lsize)")
+        bench_result = test_1_CPU(order, ns, nt, 0:255, lsize)
+
+        cpu_time = mean(bench_result.times[2:end])
+        
+        results = DataFrame(
+            "device" => "CPU", 
+            "order" => order, 
+            "# samples" => ns, 
+            "# traces" => nt,
+            "# labels" => lsize,
+            "time" => cpu_time
         )
 
         CSV.write("profiling/internal-profiling/$(output_name).csv", results, append=true)
