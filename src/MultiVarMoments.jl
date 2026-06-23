27b8fe920d34790c53f7680aeaf72b4bcce77706
george crane <gibstep99@gmail.com>
2026-06-23 18:21:41 -0400

    added fixed reference implementation for multivariate moments, included in test

--- src/MultiVarMoments.jl
+++ src/MultiVarMoments.jl
@@ -5,17 +5,18 @@ include("Utils.jl")
 include("Moments.jl")
 using .Utils, .Moments
 
-struct MultiVarMomentsAcc{Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
+# REFERENCE FUNCTIONS FOR TESTING
+struct MultiVarMomentsAccReference{Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
     totals::Ta
     SCPs::Ta  # sums of centered products
-    α::Vector{UInt}
+    α::Vector{UInt}  # order vector
     ns::UInt  # number of samples per trace (and therefore the variateness of sums of centered prods)
     nl::UInt
     _totals::Ta
     _SCPs::Ta
     _sums::Ta
 
-    function MultiVarMomentsAcc{Tt, Tl, Ta}(order::UInt, ns, nl) where {Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
+    function MultiVarMomentsAccReference{Tt, Tl, Ta}(order::UInt, ns, nl) where {Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
         totals = fill!(Ta{UInt32, 1}(undef, nl), 0)
         SCPs = fill!(Ta{Tt, 3}(undef, nl, 1, 1), 0)
         α = fill!(Ta{UInt, 1}(undef, ns), order)  # the same order is calculated for each sample position 
@@ -26,7 +27,7 @@ struct MultiVarMomentsAcc{Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
     end
 end
 
-function centered_sum_multivar!(SCPs::AbstractArray{Tt, 3}, traces::AbstractMatrix{Tt}, labels::AbstractVector{Tl}, order::AbstractVector{UInt}, means::AbstractMatrix{Tt}) where {Tt<:AbstractFloat, Tl<:Integer}
+function _centered_sum_multivar_reference!(SCPs::AbstractArray{Tt, 3}, traces::AbstractMatrix{Tt}, labels::AbstractVector{Tl}, order::AbstractVector{UInt}, means::AbstractMatrix{Tt}) where {Tt<:AbstractFloat, Tl<:Integer}
     @boundscheck begin
         checkbounds(traces, 1, size(order, 1))
         checkbounds(order, size(traces, 2))
@@ -36,6 +37,69 @@ function centered_sum_multivar!(SCPs::AbstractArray{Tt, 3}, traces::AbstractMatr
         l_i = convert(Int32, labels[i]+1)
         cp = prod((traces[i, :] .- means[l_i, :]) .^ order)
         SCPs[l_i, 1, 1] += cp
+    end
+end
+
+function _centered_sum_update_reference!(acc::MultiVarMomentsAccReference{Tt, Tl, Ta}, traces::AbstractMatrix{Tt}, labels::AbstractVector{Tl}) where {Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
+    fill!(acc._sums, 0)
+    fill!(acc._totals, 0)
+    fill!(acc._SCPs, 0)
+    
+    # Pass 1, calculate labels wise sums
+    Moments.label_wise_sum_ak!(traces, labels, acc._sums, acc._totals)
+
+    # Pass 2: find means and calculate sums of centered prods
+    means = acc._sums ./ acc._totals
+    _centered_sum_multivar_reference!(acc._SCPs, traces, labels, acc.α, means)
+
+    acc.SCPs .= acc._SCPs
+    acc.totals .= acc._totals
+end
+
+# PRACTICAL IMPLEMENTATIONS
+struct MultiVarMomentsAcc{Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
+    totals::Ta
+    SCPs::Ta  # sums of centered products
+    α::Matrix{Int}  # order vectors (vector rows)
+    ns::UInt  # number of samples per trace (and therefore the variateness of sums of centered prods)
+    nl::UInt
+    _totals::Ta
+    _SCPs::Ta
+    _sums::Ta
+
+    function MultiVarMomentsAcc{Tt, Tl, Ta}(order::Union{Int, AbstractVector{Int}, AbstractMatrix{Int}}, ns::Integer, nl::Integer) where {Tt<:AbstractFloat, Tl<:Integer, Ta<:AbstractArray}
+        if typeof(order) == Int
+            α = fill!(Ta{Int, 2}(undef, 1, ns), order)  # the same order is calculated for each sample position 
+        elseif typeof(order) <: AbstractVector{Int}
+            α = reshape(order, 1, ns)
+        else typeof(order) <: AbstractMatrix{Int}
+            checkbounds(order, 1, ns)
+            α = order
+        end
+        
+        totals = fill!(Ta{UInt32, 1}(undef, nl), 0)
+        SCPs = fill!(Ta{Tt, 3}(undef, nl, size(α, 1), 1), 0)
+
+        _totals = similar(totals)
+        _SCPs = similar(SCPs)
+        _sums = Ta{Tt, 2}(undef, nl, ns)
+        new(totals, SCPs, α, ns, nl, _totals, _SCPs, _sums)
+    end
+end
+
+function centered_sum_multivar!(SCPs::AbstractArray{Tt, 3}, traces::AbstractMatrix{Tt}, labels::AbstractVector{Tl}, order::AbstractMatrix{Int}, means::AbstractMatrix{Tt}) where {Tt<:AbstractFloat, Tl<:Integer}
+    @boundscheck begin
+        # make sure order vector and measurement vector are of equal length
+        checkbounds(traces, 1, size(order, 2)); checkbounds(order, 1, size(traces, 2))  
+    end
+    
+    for i in axes(traces, 1)
+        l_i = convert(Int32, labels[i]+1)
+        centered = traces[i, :] .- means[l_i, :]
+        for oi in axes(order, 1)
+            centered_product = prod(centered .^ order[oi, :])
+            SCPs[l_i, oi, 1] += centered_product
+        end
     end
 end
 
@@ -54,6 +118,8 @@ function centered_sum_update!(acc::MultiVarMomentsAcc{Tt, Tl, Ta}, traces::Abstr
     acc.SCPs .= acc._SCPs
     acc.totals .= acc._totals
 end
+
+
 
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,8 @@ Random.seed!(12)
 @testset "Multivariate Moment Estimation" begin
     ns = 2  # must = 2 so that covariance can be calculated from the sums of centered products
     order = 1  # must = 1 for same reason
-    m = MultiVarMoments.MultiVarMomentsAcc{Float64, UInt8, Array}(UInt(order), ns, 256)
+    mref = MultiVarMoments.MultiVarMomentsAccReference{Float64, UInt8, Array}(UInt(order), ns, 256)
+    m = MultiVarMoments.MultiVarMomentsAcc{Float64, UInt8, Array}(order, ns, 256)
     a = rand(50000, ns)  # lots of measurements are required for `cov` and `centered_sum_update` to converge
                          # since covariance is calculated per label and there are 256 labels
     l = rand(UInt8, 50000)
@@ -17,9 +18,12 @@ Random.seed!(12)
     set_0 = a[l.==0, :]
     cov_0 = cov(set_0) 
 
+    MultiVarMoments._centered_sum_update_reference!(mref, a, l)
     MultiVarMoments.centered_sum_update!(m, a, l)
 
-    @test all(.≈((m.SCPs[1, :, :] / size(a[l.==0, :], 1)), cov_0[1, 2], rtol=0.01))
+    @test all(.≈((mref.SCPs[1, :, :] / size(a[l.==0, :], 1)), cov_0[1, 2], rtol=0.01))
+    @test all(.≈(mref.SCPs, m.SCPs))
+    @test all(mref.totals .== m.totals)
 end
 
 # Test precision / stability of centered sum merging formula [Prop. 2.1, 10.2172/1028931]
