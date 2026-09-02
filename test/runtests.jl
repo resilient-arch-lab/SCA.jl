57d893719c323c00ea9257bad87043118aea1fb0
george <gibstep99@gmail.com>
2026-09-02 13:26:42 -0400

    stability test vs arbitrary precision float calculated ground truth

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using Statistics
 using Random
 
+
 # Test precision / stability of centered sum merging formula [Prop. 2.1, 10.2172/1028931]
 # with reference to a single centered sum calculation on the same data, requiring no merge
 # operation. Initial results are inconsistent even with a relative tolarance of 1. 
@@ -73,12 +74,82 @@ end
 
     println("32 bit report: ")
     println("Std. Dev. per order (averaged over sample positions and $(n) iterations):")
-    display(vec(mean(std32, dims=(1, 2, 4))))
+    display(vec(mean(std32, dims=(1, 2, 4)))')
     println("")
 
     println("64 bit report: ")
     println("Std. Dev. per order (averaged over sample positions and $(n) iterations):")
-    display(vec(mean(std64, dims=(1, 2, 4))))
+    display(vec(mean(std64, dims=(1, 2, 4)))')
+    println("")
+end
+
+@testset "Centered products estimation satability test 1: RMS error vs. float length (no merging)" begin
+    setprecision(BigFloat, 256)
+    prec = precision(BigFloat)
+    println("Ground truth float precision: $(prec)")
+    
+    NL = 2
+    a32 = rand(Float32, 20000, 20)
+    a64 = Float64.(a32)
+    a256 = big.(a64)
+    l = rand(UInt8, 20000, NL)
+
+    n = 10
+    order = 16
+    results32 = []
+    results64 = []
+
+    println("Dataset parameters: $(size(a32, 1))x$(size(a32, 2)) traces\t$(NL) label length")
+    println("Statistic parameters: centered product order = $(order)")
+    println("")
+
+    # calculate ground truth
+    m256 = Moments.UniVarMomentsAccVecLabel{BigFloat, UInt8, Array, NL}(order, size(a64, 2), 256)
+    Moments.centered_sum_update!(m256, a256, l)
+    resultsref = m256.moments
+
+    # perform `n` iterations
+    for i in 1:n
+        # estimate moments
+        m32 = Moments.UniVarMomentsAccVecLabel{Float32, UInt8, Array, NL}(order, size(a32, 2), 256)
+        m64 = Moments.UniVarMomentsAccVecLabel{Float64, UInt8, Array, NL}(order, size(a64, 2), 256)
+        Moments.centered_sum_update!(m32, a32, l)
+        Moments.centered_sum_update!(m64, a64, l)
+        
+        # store results
+        if isempty(results32)
+            results32 = m32.moments
+        else
+            results32 = cat(results32, m32.moments, dims=5)
+        end
+        if isempty(results64)
+            results64 = m64.moments
+        else
+            results64 = cat(results64, m64.moments, dims=5)
+        end
+
+        # shuffle trace rows and labels 
+        perm = randperm(size(a32, 1))
+        a32, l = SCA.TestUtils.permute_dataset_rows(a32, l, perm)
+        a64, _ = SCA.TestUtils.permute_dataset_rows(a64, l, perm)
+    end
+
+    # calculate average RMS error
+    rms32 = sqrt.(mean((resultsref .- results32).^2, dims=5))
+    rms64 = sqrt.(mean((resultsref .- results64).^2, dims=5))
+
+    println("32 bit report: ")
+    println("RMS error vs reference (averaged over sample positions and $(n) iterations):")
+    display(vec(mean(rms32, dims=(1, 2, 4)))')
+    println("Maximum RMS error: ")
+    display(vec(maximum(rms32, dims=(1, 2, 4)))')
+    println("")
+    
+    println("64 bit report: ")
+    println("RMS error vs reference (averaged over sample positions and $(n) iterations):")
+    display(vec(mean(rms64, dims=(1, 2, 4)))')
+    println("Maximum RMS error: ")
+    display(vec(maximum(rms64, dims=(1, 2, 4)))')
     println("")
 end
 
