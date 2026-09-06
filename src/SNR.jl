7ba8dcea17df9da92eeb0ccef0270b8541c85c90
george <gibstep99@gmail.com>
2026-09-06 14:48:38 -0400

    _

--- src/SNR.jl
+++ src/SNR.jl
@@ -3,7 +3,7 @@ Signal to Noise Ratio (SNR)
 """
 
 module SNR
-export SNRBasic, SNRMoments, SNROrdered, SNR_fit!, SNR_finalize
+export SNRBasic, SNRMoments, SNROrdered, SNRVecLabel, SNR_fit!, SNR_finalize
 
 include("Utils.jl")
 using .Utils
